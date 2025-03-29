7fc17009d28f98a1922ac46e9ce30d2d341e6562
Hummner <bence.cservenyak@gmail.com>
2025-03-29 22:27:49 +0100

    overlay added

---
+++ script/board.js
@@ -0,0 +1,7 @@
+function openOverlayTask() {
+    document.getElementById("board_overlay").classList.remove("d_none")
+}
+
+function closeOverlayTask() {
+    document.getElementById("board_overlay").classList.add("d_none")
+}
