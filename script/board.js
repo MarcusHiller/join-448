303454daae29a50b6c865d1c72203e150191c683
Hummner <bence.cservenyak@gmail.com>
2025-03-30 10:57:12 +0200

    Overlay Finish with Animation, font added

--- script/board.js
+++ script/board.js
@@ -1,7 +1,15 @@
-function openOverlayTask() {
+ function openOverlayTask() {
     document.getElementById("board_overlay").classList.remove("d_none")
+    setTimeout(() => {document.getElementById("overlay_container").classList.remove("overlay-container-sliding")}, 1)   
 }
 
+    
+
+   
+
+
 function closeOverlayTask() {
-    document.getElementById("board_overlay").classList.add("d_none")
+    document.getElementById("overlay_container").classList.add("overlay-container-sliding")
+    setTimeout(() => {document.getElementById("board_overlay").classList.add("d_none")}, 50)
 }
+   
