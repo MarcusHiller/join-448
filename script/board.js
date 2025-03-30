4557b315ba65b872ca15176e822e445e592c994c
Hummner <bence.cservenyak@gmail.com>
2025-03-30 19:06:52 +0200

    Add Task Overlay

--- script/board.js
+++ script/board.js
@@ -1,15 +1,37 @@
  function openOverlayTask() {
     document.getElementById("board_overlay").classList.remove("d_none")
+    document.getElementById("overlay_container").classList.remove("d_none")
     setTimeout(() => {document.getElementById("overlay_container").classList.remove("overlay-container-sliding")}, 1)   
+}
+
+function openAddTask() {
+    document.getElementById("board_overlay").classList.remove("d_none")
+    document.getElementById("add_container").classList.remove("d_none")
+    setTimeout(() => {document.getElementById("add_container").classList.remove("overlay-container-sliding")}, 1)   
 }
 
     
 
-   
+// function closeAddTask() {
+//     document.getElementById("add_container").classList.add("overlay-container-sliding")
+//     setTimeout(() => {document.getElementById("board_overlay").classList.add("d_none")}, 50);
+//     document.getElementById("add_container").classList.add("d_none")
+
+// }
 
 
 function closeOverlayTask() {
-    document.getElementById("overlay_container").classList.add("overlay-container-sliding")
-    setTimeout(() => {document.getElementById("board_overlay").classList.add("d_none")}, 50)
+    document.getElementById("overlay_container").classList.add("overlay-container-sliding");
+    setTimeout(() => {document.getElementById("board_overlay").classList.add("d_none"),
+         document.getElementById("overlay_container").classList.add("d_none")
+        }, 100);
+
 }
-   
+
+function closeAddTask() {
+    document.getElementById("add_container").classList.add("overlay-container-sliding");
+    setTimeout(() => {document.getElementById("board_overlay").classList.add("d_none"),
+         document.getElementById("add_container").classList.add("d_none")
+        }, 100);
+
+}
