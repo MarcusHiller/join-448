77926474ecc40e48014c2ad7263a1bba575ea0bd
PiaBay <pia.bay@icloud.com>
2025-04-01 23:13:15 +0200

    added structure responsive

--- script/summary.js
+++ script/summary.js
@@ -4,3 +4,23 @@ function navigateTo(url, element) {
         window.location.href = url;
     }, 80);
 }
+
+function showMetricsGrid(event) {
+    if (window.innerWidth <= 570) {
+        event.preventDefault();
+        let greeting = document.querySelector('.greeting');
+        let grid = document.querySelector('.metricsGrid');
+        let devideContent = document.querySelector('.devideContent');
+        let header = document.querySelector('.dashboardHeader');
+        if (greeting && grid) {
+            greeting.style.display = 'none';
+            grid.style.display = 'block';
+            devideContent.style.height = 'auto';
+        }
+        if (header) {
+            header.style.display = 'flex';
+        }
+        return false;
+    }
+    return true;
+}
