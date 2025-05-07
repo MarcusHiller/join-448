695ae74ecebc46de8af0ae977ee3e150f9c297f3
MarcusHiller <marcus.hiller.msg@gmail.com>
2025-05-07 17:57:47 +0200

    Capitalization edit and delete

--- script/templates.js
+++ script/templates.js
@@ -128,8 +128,8 @@ function showContact(individualContact) {
                 <div class="info-name">
                     <h4>${individualContact.username}</h4>
                     <div class="container-editing-tools">
-                        <div class="dpl-fl-al-cetr tools" onclick="editContact(${individualContact.id})"><img class="icon tools-edit" src="../assets/img/icon/edit.svg" alt=""><span>edit</span></div>
-                        <div class="dpl-fl-al-cetr tools" onclick="deleteContact(event, ${individualContact.id})"><img class="icon tools-delete" src="../assets/img/icon/delete.svg" alt=""><span>delete</span></div>
+                        <div class="dpl-fl-al-cetr tools" onclick="editContact(${individualContact.id})"><img class="icon tools-edit" src="../assets/img/icon/edit.svg" alt=""><span>Edit</span></div>
+                        <div class="dpl-fl-al-cetr tools" onclick="deleteContact(event, ${individualContact.id})"><img class="icon tools-delete" src="../assets/img/icon/delete.svg" alt=""><span>Delete</span></div>
                     </div>
                 </div>
             </div>
@@ -152,8 +152,8 @@ function showContact(individualContact) {
     </div>
     <div id="toolsRespContainer" class="tools-overlay-Container d-none" onclick="closeToolsresp()">
         <div id="toolsResp" class="editing-tools-resp d-none">
-            <div class="dpl-fl-al-cetr tools tools-resp" onclick="editRespContact(${individualContact.id})"><img class="icon tools-edit" src="../assets/img/icon/edit.svg" alt=""><span>edit</span></div>
-            <div class="dpl-fl-al-cetr tools tools-resp" onclick="deleteContact(event, ${individualContact.id})"><img class="icon tools-delete" src="../assets/img/icon/delete.svg" alt=""><span>delete</span></div>
+            <div class="dpl-fl-al-cetr tools tools-resp" onclick="editRespContact(${individualContact.id})"><img class="icon tools-edit" src="../assets/img/icon/edit.svg" alt=""><span>Edit</span></div>
+            <div class="dpl-fl-al-cetr tools tools-resp" onclick="deleteContact(event, ${individualContact.id})"><img class="icon tools-delete" src="../assets/img/icon/delete.svg" alt=""><span>Delete</span></div>
         </div>
     </div>
     `;
